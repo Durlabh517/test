2bab74f6ffd74edda3f7405e1cd73bf3b97a4b2e
Durlabh517 <42678621+Durlabh517@users.noreply.github.com>
2020-11-08 23:37:57 +0100

    Add files via upload

--- simple/src/simple/Simple.java
+++ simple/src/simple/Simple.java
@@ -21,15 +21,20 @@ public class Simple {
 		//Taking the inputs 
 		Scanner scan=new Scanner(System.in);
 		HashMap<Integer, Integer> mapStd=new HashMap<>();
+		HashMap<Integer, Integer> position=new HashMap<>();
 		System.out.println("Enter the width");
 		Integer width= scan.nextInt();
 		System.out.println("Enter the height");
 		Integer height= scan.nextInt();
-		Integer a = mapStd.put(width, height);
+		 mapStd.put(width, height);
 	    for(int id:mapStd.keySet()) {
 	    	System.out.println(id+" "+mapStd.get(id));
 	    }
-		
+	    System.out.println("Enter the x position");
+		Integer xposition= scan.nextInt();
+		System.out.println("Enter the y position");
+		Integer yposition= scan.nextInt();
+		position.put(xposition, yposition);
 		
 		
 		
